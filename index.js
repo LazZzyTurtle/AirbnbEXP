d4cf97545fc4fc527df5448b6335f9ce6c608d47
Nima Jafari Omid <109079779+LazZzyTurtle@users.noreply.github.com>
2024-10-15 18:06:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+ReactDOM.render(<h2>Hello Tets!</h2>, document.getElementById('root'));
+
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello World!!!';
+h1.className = "header";
+document.getElementById("root").append(h1);
+
